a4afeeb7d48bbdb0908b705913720f720633b39d
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-11-15 14:58:29 -0500

    indent

--- graphgps/transform/posenc_stats.py
+++ graphgps/transform/posenc_stats.py
@@ -56,33 +56,33 @@ def compute_posenc_stats(data, pe_types, is_undirected, cfg):
     else:
         undir_edge_index = to_undirected(filtered_edge_index)
 
-# Eigen values and vectors.
-evals, evects = None, None
-if 'LapPE' in pe_types or 'EquivStableLapPE' in pe_types:
-    # Convert to scipy sparse matrix
-    L = to_scipy_sparse_matrix(
-        *get_laplacian(undir_edge_index, normalization=laplacian_norm_type,
-                       num_nodes=N)
-    )
-
-    # Determine max_freqs and eigvec_norm based on PE type
-    if 'LapPE' in pe_types:
-        max_freqs = cfg.posenc_LapPE.eigen.max_freqs
-        eigvec_norm = cfg.posenc_LapPE.eigen.eigvec_norm
-    elif 'EquivStableLapPE' in pe_types:
-        max_freqs = cfg.posenc_EquivStableLapPE.eigen.max_freqs
-        eigvec_norm = cfg.posenc_EquivStableLapPE.eigen.eigvec_norm
+    # Eigen values and vectors.
+    evals, evects = None, None
+    if 'LapPE' in pe_types or 'EquivStableLapPE' in pe_types:
+        # Convert to scipy sparse matrix
+        L = to_scipy_sparse_matrix(
+            *get_laplacian(undir_edge_index, normalization=laplacian_norm_type,
+                           num_nodes=N)
+        )
     
-    # Compute only the smallest max_freqs eigenvalues and eigenvectors
-    evals, evects = eigsh(L, k=max_freqs, which='SM')
-
-    # Ensure real values for eigenvalues and eigenvectors
-    evals, evects = np.real(evals), np.real(evects)
-
-    data.EigVals, data.EigVecs = get_lap_decomp_stats(
-        evals=evals, evects=evects,
-        max_freqs=max_freqs,
-        eigvec_norm=eigvec_norm)
+        # Determine max_freqs and eigvec_norm based on PE type
+        if 'LapPE' in pe_types:
+            max_freqs = cfg.posenc_LapPE.eigen.max_freqs
+            eigvec_norm = cfg.posenc_LapPE.eigen.eigvec_norm
+        elif 'EquivStableLapPE' in pe_types:
+            max_freqs = cfg.posenc_EquivStableLapPE.eigen.max_freqs
+            eigvec_norm = cfg.posenc_EquivStableLapPE.eigen.eigvec_norm
+        
+        # Compute only the smallest max_freqs eigenvalues and eigenvectors
+        evals, evects = eigsh(L, k=max_freqs, which='SM')
+    
+        # Ensure real values for eigenvalues and eigenvectors
+        evals, evects = np.real(evals), np.real(evects)
+    
+        data.EigVals, data.EigVecs = get_lap_decomp_stats(
+            evals=evals, evects=evects,
+            max_freqs=max_freqs,
+            eigvec_norm=eigvec_norm)
 
     if 'SignNet' in pe_types:
         # Eigen-decomposition with numpy for SignNet.
