adcb64cc1daa7f00e55bbcb15f267fb5546fc014
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-11-19 18:24:24 -0500

    use true_reshaped on eval_epoch

--- graphgps/train/custom_train.py
+++ graphgps/train/custom_train.py
@@ -67,8 +67,10 @@ def eval_epoch(logger, loader, model, split='val'):
             _true = true
             _pred = pred_score
         else:
-            loss, pred_score = compute_loss(pred, true)
-            _true = true.detach().to('cpu', non_blocking=True)
+            # Reshape true for loss calculation
+            true_reshaped = true.unsqueeze(0) if true.dim() == 1 else true
+            loss, pred_score = compute_loss(pred, true_reshaped)
+            _true = true_reshaped.detach().to('cpu', non_blocking=True)
             _pred = pred_score.detach().to('cpu', non_blocking=True)
         logger.update_stats(true=_true,
                             pred=_pred,
